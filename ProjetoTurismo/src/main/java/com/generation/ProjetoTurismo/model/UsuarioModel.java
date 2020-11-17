3d5a772918b4d35820dc02ab1e2a730efcfa8870
olegario-gu <gustavolegario.sousa@gmail.com>
2020-11-17 15:26:00 -0300

    ADD Implement Security e Service

--- ProjetoTurismo/src/main/java/com/generation/ProjetoTurismo/model/UsuarioModel.java
+++ ProjetoTurismo/src/main/java/com/generation/ProjetoTurismo/model/UsuarioModel.java
@@ -1,18 +1,12 @@
 package com.generation.ProjetoTurismo.model;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -24,22 +18,58 @@ public class UsuarioModel {
 	
 	@NotNull
 	@Size(min = 2, max = 100)
-	private String nomeCompleto;
+	private String nome;
 	
 	@NotNull
-	@Size(min = 8, max = 50)
+	@Size(min = 5, max = 100)
+	private String usuario;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
 	private String senha;
 	
 	@NotNull
-	@Size(min = 2, max = 100)
+	@Size(min = 5, max = 100)
 	private String tipo;
-	
-	@NotNull
-	@Size(min = 2, max = 100)
-	private String email;
-	
-	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
-	@JsonIgnoreProperties("usuario")
-	private List<PostagemModel> postagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 	
 }
